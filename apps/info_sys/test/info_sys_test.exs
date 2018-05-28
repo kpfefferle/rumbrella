04b96ff834cb68188c2f52e09a169342f1a3dace
Kevin Pfefferle <kevin@pfefferle.co>
2018-05-28 15:38:49 -0400

    Test InfoSys compute timeout

--- apps/info_sys/test/info_sys_test.exs
+++ apps/info_sys/test/info_sys_test.exs
@@ -12,6 +12,10 @@ defmodule InfoSysTest do
     def fetch("none", ref, owner, _limit) do
       send(owner, {:results, ref, []})
     end
+    def fetch("timeout", _ref, owner, _limit) do
+      send(owner, {:backend, self()})
+      :timer.sleep(:infinity)
+    end
   end
 
   test "compute/2 with backend results" do
@@ -21,5 +25,14 @@ defmodule InfoSysTest do
 
   test "compute/2 with no backend results" do
     assert [] = InfoSys.compute("none", backends: [TestBackend])
+  end
+
+  test "compute/2 with timeout returns no results and kills workers" do
+    assert [] = InfoSys.compute("timeout", backends: [TestBackend], timeout: 10)
+    assert_receive {:backend, backend_pid}
+    ref = Process.monitor(backend_pid)
+    assert_receive {:DOWN, ^ref, :process, _pid, _reason}
+    refute_received {:DOWN, _, _, _, _}
+    refute_received :timeout
   end
 end
